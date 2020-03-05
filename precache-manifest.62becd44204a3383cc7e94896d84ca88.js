e09c25aa8d512e51e18a327a4582b5145a468ccc
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-03-05 15:59:44 +0200

    Updates

---
+++ precache-manifest.62becd44204a3383cc7e94896d84ca88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d8a2ba97be4f2283bfda371a062d09",
+    "url": "/React-HW1-component/index.html"
+  },
+  {
+    "revision": "abe30f36636eb92aa4f0",
+    "url": "/React-HW1-component/static/css/main.88a0ede2.chunk.css"
+  },
+  {
+    "revision": "fdc4c032c21f8afe9748",
+    "url": "/React-HW1-component/static/js/2.d26a421b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-HW1-component/static/js/2.d26a421b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe30f36636eb92aa4f0",
+    "url": "/React-HW1-component/static/js/main.602c8e87.chunk.js"
+  },
+  {
+    "revision": "0238d77acbe26dc04e77",
+    "url": "/React-HW1-component/static/js/runtime-main.3d79ed34.js"
+  }
+]);
